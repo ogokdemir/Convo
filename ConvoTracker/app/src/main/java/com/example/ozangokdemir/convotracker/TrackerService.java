df97dfc5f2c3668d731d6ab5bb0a6248319a2643
ogokdemir <ozan.gokdemir15@ncf.edu>
2019-03-15 14:09:00 -0400

    added some comments to the code.

--- ConvoTracker/app/src/main/java/com/example/ozangokdemir/convotracker/TrackerService.java
+++ ConvoTracker/app/src/main/java/com/example/ozangokdemir/convotracker/TrackerService.java
@@ -4,7 +4,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
-
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
@@ -16,27 +15,25 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.Manifest;
-import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.widget.Toast;
+
 
 public class TrackerService extends Service {
 
     private static final String TAG = TrackerService.class.getSimpleName();
     public static final String INTENT_RECEIVE_CODE = "99";
     String mEmail, mPassword;
+    FirebaseAuth mAuth;
 
     @Override
     public IBinder onBind(Intent intent) {return null;}
@@ -44,7 +41,10 @@ public class TrackerService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mAuth = FirebaseAuth.getInstance(); // get a hold of the firebase auth api reference.
         buildNotification();
+
     }
 
     /*
@@ -54,18 +54,11 @@ public class TrackerService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-
-        Log.i(TAG, "onStartCommand called.");
-
         Bundle received = intent.getExtras();
         String[] emailpassword = received.getStringArray(INTENT_RECEIVE_CODE);
 
         mEmail = emailpassword[0];
         mPassword = emailpassword[1];
-
-        Log.i(TAG, "did the array arrive: "+ String.valueOf(emailpassword==null));
-        Log.i(TAG, "Email: "+ emailpassword[0]);
-        Log.i(TAG, "Password: "+ emailpassword[1]);
 
         loginToFirebase();
 
@@ -73,9 +66,17 @@ public class TrackerService extends Service {
 
     }
 
+    /**
+     * This buils the notification that displays "you're being tracked."
+     * This also registers the broadcast receiver which starts streaming data from the GPS manager of the device.
+     * Creates a pending intent that activates and unregisters the GPS stream when the user taps on the notification.
+     */
     private void buildNotification() {
         String stop = "stop";
-        registerReceiver(stopReceiver, new IntentFilter(stop));
+        registerReceiver(stopReceiver, new IntentFilter(stop)); //start listening to data updates from the GPS.
+
+
+        //This pending intent will be activated when the user taps on the notification.
         PendingIntent broadcastIntent = PendingIntent.getBroadcast(
                 this, 0, new Intent(stop), PendingIntent.FLAG_UPDATE_CURRENT);
         // Create the persistent notification
@@ -88,6 +89,7 @@ public class TrackerService extends Service {
         startForeground(1, builder.build());
     }
 
+    //
     protected BroadcastReceiver stopReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
@@ -98,21 +100,32 @@ public class TrackerService extends Service {
 
             /**
              * Here I should add the code to remove the reference to the firebase database.
+             * The entry on the Firebase database must be deleted.
+             *
+             * Write log tests to see whether mAuth.getCurrent user returns a name and last name. If it does,
+             * use that to delete this entry from the firebase database.
              */
+
+
+
+
         }
     };
 
+    //Uses the member variables mEmail and mPassword to login to Firebase. These member variables are passed from the TrackerActivity.
     private void loginToFirebase() {
         // Authenticate with Firebase, and request location updates
 
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+        mAuth.signInWithEmailAndPassword(
                 mEmail, mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(Task<AuthResult> task) {
+                //if the login was successful, start tracking the user's location.
                 if (task.isSuccessful()) {
                     Log.d(TAG, "firebase auth success");
                     Toast.makeText(TrackerService.this, "Success, tracker activated!", Toast.LENGTH_SHORT).show();
                     requestLocationUpdates();
+                //If the login fails, notify user.
                 } else {
                     Log.d(TAG, "firebase auth failed");
                 }
@@ -127,9 +140,6 @@ public class TrackerService extends Service {
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY); //use both the GPS and Wifi, Blueetooth for best accuracy.
         FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
 
-        /**
-         * Another problem is getting the user's name. For now, I will use their email as the key and display that on the map icon.
-         */
 
         final String path = getString(R.string.firebase_path) + "/" + extractUsersNameFromNcfEmail(mEmail);
 
@@ -155,6 +165,7 @@ public class TrackerService extends Service {
 
                         //set the value of the location path in the realtime database to the latest location.
                         ref.setValue(location);
+
                     }
                 }
             }, null);
@@ -162,17 +173,15 @@ public class TrackerService extends Service {
     }
 
     /**
+     * This should probably be in a util class but let it stay around for now. Extracts name and last name from ncf email as key to Firebase.
      * @param email NCF email address of the user.
      * @return extracts their name and last name from the NCF email address and returns it.
      */
     private String extractUsersNameFromNcfEmail(String email){
         String[] chunks = email.split("@");
         String[] fNamelName = chunks[0].split("\\.");
-
         String firstName = fNamelName[0].substring(0,1).toUpperCase()+fNamelName[0].substring(1);
         String lastName =  fNamelName[1].substring(0,1).toUpperCase()+fNamelName[1].substring(1,fNamelName[1].length()-2);
-
         return firstName+" "+lastName;
-
     }
 }
