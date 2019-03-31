989b4c995d54e4eb555349fd2aad03ad55b8f247
ogokdemir <ozan.gokdemir15@ncf.edu>
2019-03-30 23:18:47 -0400

    Added splash screen opening to both the apps with their logos. Development is complete. The app is ready to deploy.

--- ConvoTracker/app/src/main/java/com/example/ozangokdemir/convotracker/SplashScreenActivity.java
+++ ConvoTracker/app/src/main/java/com/example/ozangokdemir/convotracker/SplashScreenActivity.java
@@ -1,5 +1,7 @@
 package com.example.ozangokdemir.convotracker;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
+        /* New Handler to start the LoginActivity
+         * and close this Splash-Screen after 3.5 seconds.*/
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashScreenActivity.this,LoginSignupActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, 3500);
     }
 }
