cce382540d8f50a40374c63d2c2386b300ab31f0
ogokdemir <ozan.gokdemir15@ncf.edu>
2019-03-30 21:56:21 -0400

    Revolutionized the UI design. Convo apps now look beautiful!

--- ConvoMap/app/src/main/java/com/example/ozangokdemir/convomap/SurveysActivity.java
+++ ConvoMap/app/src/main/java/com/example/ozangokdemir/convomap/SurveysActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
